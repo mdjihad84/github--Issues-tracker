b8a7682d6169db9755c56e41dad01c909e949bf6
MD JIHAD <admin@gmail.com>
2026-03-07 16:23:39 +0600

    complete login page functionality

--- script/app.js
+++ script/app.js
@@ -1,1 +1,27 @@
-console.log('hellow world');
+const singBtn = document.getElementById("btn-signIn");
+
+singBtn.addEventListener('click', (event) => {
+
+    event.preventDefault()
+
+    const userNameField = document.getElementById("signIn-userName");
+    const userPasswordField = document.getElementById("signIn-password");
+
+    const userValue = userNameField.value.trim();
+    const passwordValue = userPasswordField.value.trim();
+
+    if(userValue !== "admin"){
+        alert('Invalid userName! please try again')
+        return
+    }
+
+    // check password
+    if (passwordValue !== "admin123") {
+      alert("Invaild password! please try again");
+      return;
+    }
+
+    alert('Login Succesefull!')
+
+    window.location.href = 'home.html';
+});
