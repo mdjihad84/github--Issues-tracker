8362a8f96570327959f871eb1c315ff299c5e98a
MD JIHAD <admin@gmail.com>
2026-03-08 22:29:58 +0600

    fixed dynamic card simple error

--- script/home.js
+++ script/home.js
@@ -29,6 +29,7 @@ const serverData = (recevedData) => {
   recevedData.forEach((item) => {
     const createElement = document.createElement("div");
     createElement.innerHTML = `
+
         <div class="max-w-6xl mx-auto mb-4">
             <div onclick="openModal('${item.id}')" class="cursor-pointer bg-white rounded-2xl shadow-sm hover:shadow-md transition-shadow 
                 ${item.status === "closed" ? "border-t-4 border-purple-500" : "border-t-4 border-emerald-500"}">
@@ -130,21 +131,30 @@ const openModal = (id) => {
           <h2 class="text-2xl font-bold mb-4">${modalData.title}</h2>
           <div class="flex gap-2 mb-4">
               <span class="badge badge-success">${modalData.status}</span>
-              <span class="badge badge-error">${modalData.priority}</span>
           </div>
           
           <p class="text-slate-500 mb-6">${modalData.description}</p>
 
-          <div class="flex flex-wrap gap-2 mb-5">${modalData.labels.map((label) => `
+          <div class="flex flex-wrap gap-2 mb-5">${modalData.labels
+            .map(
+              (label) => `
                 <div class="bg-yellow-500 border-slate-200 text-black text-[12px] pr-3 rounded-lg gap-1.5 uppercase font-bold h-auto">
                     <i class="fa-solid ${label.toLowerCase().includes("bug") ? "fa-bug text-red-400" : "fa-circle-info text-amber-400"}"></i>${label}
-                </div>`).join("")}
+                </div>`,
+            )
+            .join("")}
           </div>
-          
-          <div class="p-4 bg-slate-50 rounded-lg">
-              <p><strong>Assignee:</strong> ${modalData.assignee || "None"}</p>
-              <p><strong>Created:</strong> ${new Date(modalData.createdAt).toLocaleDateString()}</p>
-          </div>`;
+          <div class="p-4 bg-slate-50 rounded-lg flex">
+                <div class="">
+                    <p><strong>Assignee:</strong> ${modalData.assignee || "None"}</p>
+                    <p><strong>Created:</strong> ${new Date(modalData.createdAt).toLocaleDateString()}</p>
+                </div>
+                <div class="mb-2 ml-32">
+                    <p>Priority:</p>
+                    <span class="badge badge-error">${modalData.priority}</span>
+                </div>
+            </div>
+          `;
       document.getElementById("issue_modal").showModal();
     })
 };
